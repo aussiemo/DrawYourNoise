4cf5352e2496a41fe1724b1fc6c728b2b6436226
Moritz Jasper <anon@test.com>
2018-05-09 18:34:11 +0200

    Generating Buffers and some arbitrary data @wip

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "E:\Programme\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl main.cpp
+cl main.cpp /link openal\libs\Win64\OpenAL32.lib
--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include "openal/include/al.h"
 #include "openal/include/alc.h"
 
+ALubyte pcmData[1024];
 
+void fillPcmData() {
+	for (int i = 0; i < 1024; ++i) {
+		pcmData[i] = 128 + ((i%128) - 64);
+	}
+}
 
 int main(int argc, char* argv[]) {
-	auto defaultDevice = alcOpenDevice(0);
-	auto closeWasSuccessful = alcCloseDevice(defaultDevice); 
-	// for fun: alcCloseDevice((ALCdevice *)1); this causes crash instead of returning ALC_FALSE
+	auto device = alcOpenDevice(0);
+	auto context = alcCreateContext(device, 0);
+	alcMakeContextCurrent(context);
+	
+	ALuint *buffers = nullptr;
+	alGenBuffers(1, buffers);
+	
+	alBufferData(buffers[0], 
+					AL_FORMAT_MONO8, 
+					data, 
+					size, 
+					freq);
+	
+	alcCloseDevice(device);
 	
 	if (ALC_TRUE == closeWasSuccessful) {
 		return 0;
